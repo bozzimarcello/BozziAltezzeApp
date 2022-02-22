d92ce216d9c42312df89877fd0dd3968d8d642e1
Marcello Bozzi <bozzimarcello@gmail.com>
2022-02-22 09:46:45 +0100

    Aggiungere i file di progetto.

---
+++ BozziAltezzeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BozziAltezzeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ricerca dei più alti");
+
+            // dichiaro un vettore di 20 elementi
+            int[] altezze = new int[20];
+
+            for(int i=0; i<20; i++)
+            {
+                Console.WriteLine("Inserisci l'altezzza della persona:");
+                altezze[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+}
